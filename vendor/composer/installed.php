c5964f49d7fbf88b8e874c45765e5759da5c8657
rogercastilloes <166962942+rogercastilloes@users.noreply.github.com>
2025-06-25 19:03:33 +0200

    Add vendor/composer/installed.php 1750871012600

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'katie-bray/wordpress-theme',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5c3942bb54826c037a6ac283039287251226af24',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'katie-bray/wordpress-theme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5c3942bb54826c037a6ac283039287251226af24',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.21.0',
+            'version' => '10.21.0.0',
+            'reference' => 'b4ab319731958077227fad1874a3671458c5d593',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
